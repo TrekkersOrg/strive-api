9d384cb732148e7e63bea7dc293b9bd1e287f542
devenshah2018 <46404643+devenshah2018@users.noreply.github.com>
2024-06-28 01:04:36 -0700

    Added /mongo/getuser API endpoint for login.

---
+++ Models/MongoDB-GetUser-Response.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace strive_api.Models
+{
+    public class MongoDB_GetUser_Response
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("username")]
+        public string? Username { get; set; }
+
+        [BsonElement("password")]
+        public string? Password { get; set; }
+    }
+}
